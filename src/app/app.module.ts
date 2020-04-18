01cb61006b87c603b11dad4f2f98587ce2038076
Renan Zazula <renanzazula@gmail.com>
2020-04-18 15:05:45 +0200

    Clean Version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,29 +6,16 @@ import {FormsModule, ReactiveFormsModule} from "@angular/forms";
 import {DashboardComponent} from './dashboard/dashboard.component';
 import {HeaderComponent} from './header/header.component';
 import {MenuComponent} from "./menu/menu.component";
-import {MarcaComponent} from './gerenciar/marca/marca.component';
-import {MarcaListaComponent} from './gerenciar/marca/marca-lista/marca-lista.component';
-import {DialogComponent} from "./mensagens/dialog/dialog.component";
 import {MatDialog, MatDialogModule} from "@angular/material";
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {MarcaCadastrarComponent} from './gerenciar/marca/marca-cadastrar/marca-cadastrar.component';
-import {AlertaComponent} from './mensagens/alerta/alerta.component';
 import {AppRoutingModule} from "./app.routing";
-import {AlertaService} from "./service/mensagens/alerta/alerta.service";
-
 
 @NgModule({
     declarations: [
         AppComponent,
         DashboardComponent,
-        HeaderComponent,
         MenuComponent,
-        HeaderComponent,
-        MarcaComponent,
-        MarcaListaComponent,
-        DialogComponent,
-        MarcaCadastrarComponent,
-        AlertaComponent
+        HeaderComponent
     ],
     imports: [
         BrowserModule,
@@ -39,8 +26,8 @@ import {AlertaService} from "./service/mensagens/alerta/alerta.service";
         BrowserAnimationsModule,
         AppRoutingModule
     ],
-    entryComponents: [DialogComponent],
-    providers: [AlertaService, MatDialog],
+    entryComponents: [],
+    providers: [MatDialog],
     bootstrap: [AppComponent]
 })
 export class AppModule {
--- src/app/mensagens/dialog/dialog-data.interface.ts
+++
@@ -1,7 +0,0 @@
-export interface DialogDataInterface {
-    cabecalho: string // Exclusao - danger , edicao - alert
-    tipo: string;
-    mensagem: string;
-    codigo: string;
-    nome: string;
-}
